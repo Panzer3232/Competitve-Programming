8907955ea67eb5183337a86d1cb043a15fc2d58c
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-04-22 01:10:23 +0530

    Add files via upload

---
+++ Programming/CodeForces/Candies.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC push_options
+#pragma GCC optimize ("unroll-loops")
+ 
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x,y) cout <<(#x)<<" is "<<(x)<<" and "<<(#y)<<" is "<<(y)<<"\n"
+ 
+#define pow2(x) ((x)*(x))
+#define ll long long
+#define ld long double
+#define eb emplace_back
+#define pb push_back
+#define pf push_front
+ 
+#define mod 1000000007
+#define clock (clock() * 1000.0 / CLOCKS_PER_SEC)
+ 
+#define mp make_pair
+#define ff first
+#define ss second
+#define null NULL
+ 
+#define all(c) (c).begin(),(c).end()
+#define nl "\n"
+ 
+typedef vector<ll> vl;
+typedef vector< vl > vvl;
+typedef pair< ll,ll> pll;
+typedef map< ll,ll> mll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll x,k,n;
+        cin>>n;
+         x=k=1;
+       while(true){
+           x=x*2;
+           k+=x;
+           if(n%k==0){
+               cout<<n/k<<endl;
+               break;
+           }
+       }
+       
+}
+
+    return 0;
+}
