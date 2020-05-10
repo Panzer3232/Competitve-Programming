495e1d298c51908d93ee96b7863c3a4704331c3c
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-10 14:06:52 +0530

    Add files via upload

---
+++ Leetcode/RemoveDuplicatesFromArray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    return unique(nums.begin(), nums.end()) - nums.begin();
+    }
+}; 
+
+OR 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     if(nums.size()==0)
+     return 0;
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+};
