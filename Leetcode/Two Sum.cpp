a724f3be408a70a25a221634129da9451b45183c
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-24 21:04:09 +0530

    Update Two Sum.cpp

--- Leetcode/Two Sum.cpp
+++ Leetcode/Two Sum.cpp
@@ -10,3 +10,21 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+   class Solution {
+    public:
+ vector<int> twoSum(vector<int> &numbers, int target)
+ {
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+		if (hash.find(numberToFind) != hash.end()) {
+			result.push_back(hash[numberToFind] );
+			result.push_back(i);			
+			return result;
+		}
+		hash[numbers[i]] = i;
+	}
+	return result;
+}
+    };
