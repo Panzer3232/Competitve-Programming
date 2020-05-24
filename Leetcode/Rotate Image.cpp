deaf04a905794e0800471f1a7a1bf2d262af2455
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-24 21:05:31 +0530

    Update Rotate Image.cpp

--- Leetcode/Rotate Image.cpp
+++ Leetcode/Rotate Image.cpp
@@ -40,3 +40,16 @@ rotate the input matrix in-place such that it becomes:
   [16, 7,10,11]
 ]
 */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+     int n=matrix.size();
+        for(auto i=0;i<n;i++){
+            for(auto j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+         for(auto i=0;i<n;i++)
+             reverse(matrix[i].begin(),matrix[i].end());
+    }
+};
