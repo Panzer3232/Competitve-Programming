1bc752485761449a76ef47ee43134650ebf585a9
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-24 21:08:02 +0530

    Create Number of cycles.cpp

---
+++ Hackerearth/Number of cycles.cpp
@@ -0,0 +1,31 @@
+/*
+You are given an N-sided regular polygon. You have connected the center of the polygon with all the vertices, thus dividing the polygon into N equal parts.
+
+Your task is to find the count of simple cycles that exist in the modified structure of the polygon.
+
+Input format
+
+The first line contains an integer  denoting the number of queries.
+The second line contains an integer  denoting the number of sides in the regular polygon.
+Output format
+
+Print the count of simple cycles corresponding to each query in a separate line.
+
+Constraints
+ 1<=N<=10 power 9
+ 1<=Q<=10 power 5
+
+SAMPLE INPUT 
+2
+3
+4
+SAMPLE OUTPUT 
+7
+13
+Explanation
+No Explanation Provided
+
+Time Limit:	1.0 sec(s) for each input file.
+Memory Limit:	256 MB
+Source Limit:	1024 KB
+BEST SUBMISSION
