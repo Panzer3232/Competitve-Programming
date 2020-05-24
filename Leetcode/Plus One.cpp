be2030e1bf077001f468a8002722ca7348edc486
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-24 21:01:55 +0530

    Update Plus One.cpp

--- Leetcode/Plus One.cpp
+++ Leetcode/Plus One.cpp
@@ -16,3 +16,23 @@ Input: [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 */
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        digits[n-1]=1+digits[n-1];
+        int carry =digits[n-1]/10;
+        digits[n-1]=digits[n-1]%10;
+        for(auto i=n-2;i>=0;i--){
+            if(carry==1){
+                 digits[i]=1+digits[i];
+                 carry =digits[i]/10;
+                 digits[i]=digits[i]%10;
+            }
+        }
+        if(carry==1){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
