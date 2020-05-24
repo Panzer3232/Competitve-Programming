0464d41802025fd314b0fd5690a0b9dfaad71dd0
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-24 21:08:31 +0530

    Update Number of cycles.cpp

--- Hackerearth/Number of cycles.cpp
+++ Hackerearth/Number of cycles.cpp
@@ -29,3 +29,55 @@ Time Limit:	1.0 sec(s) for each input file.
 Memory Limit:	256 MB
 Source Limit:	1024 KB
 BEST SUBMISSION
+*/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC push_options
+#pragma GCC optimize ("unroll-loops")
+ 
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x,y) cout <<(#x)<<" is "<<(x)<<" and "<<(#y)<<" is "<<(y)<<"\n"
+ 
+#define pow2(x) ((x)*(x))
+#define ll long long
+#define ld long double
+#define eb emplace_back
+#define pb push_back
+#define pf push_front
+ 
+#define mod 1000000007
+#define clock (clock() * 1000.0 / CLOCKS_PER_SEC)
+ 
+#define mp make_pair
+#define ff first
+#define ss second
+#define null NULL
+ 
+#define all(c) (c).begin(),(c).end()
+#define nl "\n"
+ 
+typedef vector<ll> vl;
+typedef vector< vl > vvl;
+typedef pair< ll,ll> pll;
+typedef map< ll,ll> mll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    ll N;
+    cin >> N;
+    while(N--)
+    {
+     ll Q;
+	 cin>>Q;
+	 ll cycle;
+	 cycle=(Q*(Q-1))+1;
+	 cout<<cycle<<" ";cout<<endl;
+}
+
+    return 0;
+}
