900720b20a30bf1e36f2957482cae9063a8666c7
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-28 00:00:04 +0530

    Update Reverse Integer.cpp

--- Leetcode/Reverse Integer.cpp
+++ Leetcode/Reverse Integer.cpp
@@ -17,3 +17,17 @@ Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2 pow 31,  2 pow 31 − 1].
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
+class Solution {
+public:
+    int reverse(int x) {
+        long long int currNum=0,revNum=0;
+        while(x!=0){
+            currNum = x%10;
+             x=x/10;
+            if((revNum >INT_MAX/10) || (revNum == INT_MAX && currNum >7)) return 0;
+            if((revNum <INT_MIN/10) || (revNum == INT_MIN && currNum <-8)) return 0;  
+             revNum=revNum *10+currNum;
+        }
+        return revNum; 
+    }
+};
