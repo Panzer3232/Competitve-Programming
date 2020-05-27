59d49ddfbffdc8c1eaa0039363eff75ca7365017
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-27 15:42:02 +0530

    Create Longest Repeated Subsequence.cpp

---
+++ DP/Longest Repeated Subsequence.cpp
@@ -0,0 +1,11 @@
+/*
+Input: str = "aabb"
+Output: "ab"
+
+Input: str = "aab"
+Output: "a"
+The two subsequence are 'a'(first) and 'a' 
+(second). Note that 'b' cannot be considered 
+as part of subsequence as it would be at same
+index in both.
+*/
