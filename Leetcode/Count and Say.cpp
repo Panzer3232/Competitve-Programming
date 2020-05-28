701e9371084ab9aa321d24243fbedcd3a4c5800e
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-05-29 00:51:48 +0530

    Update Count and Say.cpp

--- Leetcode/Count and Say.cpp
+++ Leetcode/Count and Say.cpp
@@ -13,4 +13,41 @@ The count-and-say sequence is the sequence of integers with the first five terms
 Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
 
 Note: Each term of the sequence of integers will be represented as a string.
+
+Example 1:
+
+Input: 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: 4
+Output: "1211"
+Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency
 */
+class Solution {
+public:
+    string countAndSay(int n) {
+       if(n==1) return "1";
+        if(n==2) return "11";
+        string p="11";
+       for(auto i=3;i<=n;i++){
+           p+= '@'; 
+       int len=p.length(),cnt=1;
+        string temp="";
+        for(auto j=1;j<len;j++){
+            if(p[j]!=p[j-1]){
+                temp+=cnt+'0';
+                temp+=p[j-1];
+                cnt=1;
+            }
+            else{
+                cnt++;
+            cout<<cnt<<endl;
+            }
+        }
+        p=temp;
+    }
+    return p;
+}
+};
