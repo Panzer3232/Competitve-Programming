63e8269d814fed42ed948807aab854e962c8e772
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-06-24 23:53:57 +0530

    Update duplicates in Binary Search Tree.cpp

--- Tree DS/duplicates in Binary Search Tree.cpp
+++ Tree DS/duplicates in Binary Search Tree.cpp
@@ -1,3 +1,26 @@
+/*
+12(3)
+       /        \
+     10(2)      20(1)
+    /    \       
+ 9(1)   11(1)   
+
+Count of a key is shown in bracket
+This approach has following advantages over above simple approach.
+
+1) Height of tree is small irrespective of number of duplicates. Note that most of the BST operations (search, insert and delete) have time complexity as O(h) where h is height of BST. 
+So if we are able to keep the height small,we get advantage of less number of key comparisons.
+
+2) Search, Insert and Delete become easier to do. We can use same insert, search and delete algorithms with small modifications (see below code).
+
+3) This approach is suited for self-balancing BSTs (AVL Tree, Red-Black Tree, etc) also. These trees involve rotations, and a rotation may violate BST property of simple 
+solution as a same key can be in either left side or right side after rotation.
+
+Below is implementation of normal Binary Search Tree with count with every key. This code basically is taken from code for insert and delete in BST. The changes made for 
+handling duplicates are highlighted, rest of the code is same.
+
+*/
+
 #include<bits/stdc++.h> 
 
 using namespace std;
