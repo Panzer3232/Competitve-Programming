a8dd1531a76c734b3c1d0fff9970fc3684e3946f
Anmol Ashri <35276025+Panzer3232@users.noreply.github.com>
2020-07-23 00:00:48 +0530

    Update PTMSSNG.cpp

--- CodeChef/PTMSSNG.cpp
+++ CodeChef/PTMSSNG.cpp
@@ -44,3 +44,74 @@ fig1
 
 All submissions for this problem are available.
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC push_options
+#pragma GCC optimize ("unroll-loops")
+ 
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x,y) cout <<(#x)<<" is "<<(x)<<" and "<<(#y)<<" is "<<(y)<<"\n"
+#define MAX 100000
+#define pow2(x) ((x)*(x))
+#define ll long long
+#define ld long double
+#define eb emplace_back
+#define pb push_back
+#define pf push_front
+ 
+#define mod 1000000007
+#define clock (clock() * 1000.0 / CLOCKS_PER_SEC)
+ 
+#define mp make_pair
+#define ff first
+#define ss second
+#define null NULL
+ 
+#define all(c) (c).begin(),(c).end()
+#define nl "\n"
+ 
+typedef vector<ll> vl;
+typedef vector< vl > vvl;
+typedef pair< ll,ll> pll;
+typedef map< ll,ll> mll;
+
+int firstNonRepeating(vector<ll>arr, ll n) 
+{ 
+    map<ll, ll> mp; 
+    for (auto i = 0; i < n; i++) 
+        mp[arr[i]]++; 
+        
+    for (auto i = 0; i < n; i++) 
+        if (mp[arr[i]]%2== 1) 
+            return arr[i]; 
+    return 0; 
+} 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        vector<ll>a1;
+        vector<ll>a2;
+        
+        for(auto i=1;i<=4*n-1;i++){
+            ll x,y;
+            cin>>x>>y;
+            a1.eb(x);
+            a2.eb(y);
+        }
+        ll n1=a1.size();
+        ll n2 =a2.size();
+        cout<<firstNonRepeating(a1,n1)<<" "<<firstNonRepeating(a2,n2)<<" ";
+        cout<<nl;
+    }
+    return 0;
+}
